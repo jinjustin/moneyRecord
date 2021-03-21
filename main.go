790d0cbb9b30c4272a4fbbb751bb82dc31f43c13
jinjustin <jindefery@gmail.com>
2021-03-22 01:51:50 +0700

    update connect function
    - MoneySaver() function use to return moneySaver struct
    - Connect(projectID string, credentialsFile string) use to connect to firebase
    - Close() use to close connection to firebase
    -

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"	
+	//"github.com/gin-gonic/gin"
+	"github.com/jinjustin/moneyRecord/moneySaver"
+	"log"	
+)
+
+var (
+	projectID = "moneyrecord-7ef16"
+	credentialsFile = "./moneySaver/moneyrecord-7ef16-firebase-adminsdk-ymi9q-56a433392f.json"
 )
 
 func main() {
-	r := gin.Default()
+	/*r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run()*/
+
+	m := moneySaver.MoneySaver()
+
+	err := m.Connect(projectID, credentialsFile)
+	if err != nil{
+		log.Fatalf("Failed to connect: %v\n", err)
+	}
+	defer m.Close()
+
+	err = m.Test()
+	if err != nil{
+		log.Fatalf("Failed to add new record: %v\n", err)
+	}
 }
